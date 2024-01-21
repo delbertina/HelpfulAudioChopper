e984a0429707976caea96782ce82c2cb4f14f70d
delbertina <6349928+delbertina@users.noreply.github.com>
2024-01-21 17:58:28 -0600

    Add dialog to choose exported file type

--- src/main.py
+++ src/main.py
@@ -1,16 +1,10 @@
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
-from tkinter import Tk
+from tkinter import Tk, simpledialog
 from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import askyesno
 from pathlib import Path
 from datetime import datetime
-
-# Define a function to normalize a chunk to a target amplitude.
-def match_target_amplitude(aChunk, target_dBFS):
-    ''' Normalize given audio chunk '''
-    change_in_dBFS = target_dBFS - aChunk.dBFS
-    return aChunk.apply_gain(change_in_dBFS)
 
 # make directory for output files
 def prepare_output_dir() -> str:
@@ -24,8 +18,6 @@ Tk().withdraw() # we don't want a full GUI, so keep the root window from appeari
 filename = askopenfilename(
     title="Choose file to start chopping",
     filetypes=[('audio files', '.mp3;.wav')]) # show an "Open" dialog box and return the path to the selected file
-short_filename = (filename.split('/')[-1])[:16]
-print(filename, short_filename)
 
 # Load your audio.
 song = ""
@@ -40,8 +32,8 @@ else:
 chunks = split_on_silence (
     # Use the loaded audio.
     song, 
-    min_silence_len = 200,
-    silence_thresh = -60,
+    min_silence_len = 150,
+    silence_thresh = -90, # -96 is the lowest possible dBFS value
     keep_silence = 100
 )
 
@@ -53,14 +45,33 @@ confirm_answer = askyesno(title=(str(len(chunks)) + " Chunks Found"),
 if (not confirm_answer):
     exit("User decided to not continue.")
     
+# get the file name from the file path and truncate to 16 characters    
+short_filename = (filename.split('/')[-1])[:16] 
+# confirm file prefix with user
+file_prefix = simpledialog.askstring(
+    title = "Confirm File Prefix",
+    prompt = "Please enter the desired file prefix",
+    initialvalue=short_filename)
+
+# if file prefix is not empty, add a dash after
+if (file_prefix):
+    file_prefix += "-"
+    
+output_format_int = simpledialog.askinteger(title="File Format",
+                                 prompt="Please select the desired output file format.\n1 = .wav\n2 = .mp3",
+                                 initialvalue=1,
+                                 minvalue=1, maxvalue=2)  
+output_format = "wav" if output_format_int == 1 else "mp3"
+print('output format dialog value: ', output_format_int, output_format)
+
 folder_path = prepare_output_dir()
 
-# Process each chunk with your parameters
+# Process each chunk with parameters
 for i, chunk in enumerate(chunks):
     # Export the audio chunk
-    print("Exporting chunk{0}.wav.".format(i))
+    print("Exporting chunk{0}.".format(i) + output_format)
     chunk.export(
-        folder_path + "/" + short_filename + "-" + "chunk{0}.wav".format(i),
+        folder_path + "/" + file_prefix + "chunk{0}.".format(i) + output_format,
         bitrate = "192k",
-        format = "wav"
+        format = output_format
     )
